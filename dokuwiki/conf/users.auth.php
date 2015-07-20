ab1f9bef5353691825859d734fd9df65c603dd8c
Pluto <xyan@gmx.net>
2015-07-20 22:27:49 +0200

    added dokuwiki data

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+xyan:$1$PI2Wva4f$iw98OV0ElpZoy/ZYgKMpO.:Xyan:xyan@gmx.net:admin,user
